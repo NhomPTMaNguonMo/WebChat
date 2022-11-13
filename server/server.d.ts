4c0aaf7d2cad3f4dabdd866f2dedb2c80889ae54
Hồ Thanh Cương <hothanhh.cuong45@gmail.com>
2022-11-13 19:58:15 +0700

    ss

--- server/server.d.ts
+++
@@ -1,5 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-import { Server } from "socket.io";
-declare const io: Server<import("socket.io/dist/typed-events.js").DefaultEventsMap, import("socket.io/dist/typed-events.js").DefaultEventsMap, import("socket.io/dist/typed-events.js").DefaultEventsMap, any>;
-export declare function Vali(req: Request, res: Response, next: NextFunction): Promise<void>;
-export default io;
